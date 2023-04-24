fdc369f15d69d74206343eb2bdf94a7820422384
jeezOC <ale.ob02@outlook.com>
2023-04-24 02:28:22 -0600

    esto no sirve perdon

--- CILK/init.c
+++ CILK/init.c
@@ -3,52 +3,72 @@
 #include <math.h>
 #include <cilk/cilk.h>
 
-
-void intento (int rank,int size, int *sum){
-  printf("hello im fakerank=>%d from P=>%d\n",rank,  __cilkrts_get_worker_number());
-
-  int my_value=0;
-  int *valueRecived = &my_value;
-  int random = (rand() % 100 * rank+1);
-
-  printf("my_rand id=> %d\n", random);
-
-  for (int i = 0; i <= log2(size); i++) {
-
-    if (rank % (int) pow(2, i) == (int) pow(2, i) - 1) {
-      //        printf("esta mierda tiene %d\n", *valueRecived);
-      //recive(&valueRecived);
-      int index = rank - (int)pow(2,i-1);
-      printf("Rank_%d recive %d ->  Rank_%d \n ",rank,*valueRecived, index);
-      sum += *valueRecived;     
-    }
-
-    if (rank % (int) pow(2, i) == (int) pow(2, i) - (int) pow(2, i - 1) - 1) {
-      int index = rank + (int)pow(2,i-1);
-      //int aux = randoms[index];
-      //send(randoms[index]);
-      *valueRecived = random;
-      printf("Rank_%d envia %d ->  Rank_%d \n ",rank,*valueRecived, index);
-    }
-  }
+int powParser(int base, int exponent) {
+  return (int)pow((double)base, (double)exponent);
 }
 
 int main() { 
-  int size =4;
+  int size =5;
   int randoms[size];
-
+  int mail[size];
   //Inicializar el arreglo con valores aleatorios
- // for (int i = 0; i < size; i++) {
-   // randoms[i] = rand() % 100;
-  //}
+  for (int i = 0; i < size; i++) {
+    int random = (rand() % 50 * i+3);
+    printf("load rand in %d = %d\n", i, random);
+    randoms[i] = random;
+  }
   // Sumar los valores del arreglo en paralelo
   int sum = 0;
+
+  int valR = 0;
+  int origin = 0;
+  int dest = 0;
 #pragma cilk grainsize =1
   cilk_for(int rank = 0; rank< size; ++rank){
-    cilk_spawn intento(rank,size, &sum);
+    //  printf("hello from P=>%d\n",  __cilkrts_get_worker_number());
+    int my_value=0, vr=0, o=0, d=0;
+    //int *sum = &my_value;
+
+    // printf("my_rand %d\n", randoms[rank]);
+
+    //for(int j = 0; j<size; ++j){
+    // if(rank ==j){
+    for (int i = 0; i <= log2(size); i++) {
+
+      if ((rank % powParser(2, i)) == (powParser(2, i) - 1)) {
+        //        printf("esta mierda tiene %d\n", *valueRecived);
+        //recive(&valueRecived)
+        //;
+
+        if(origin  == (rank - powParser(2,i-1))){
+          if(dest == rank && origin !=rank){
+            //*valR = randoms[origin];
+            printf("Rank_%d <- Rank_%d => %d \n",dest,origin,mail[rank]);
+            sum+= mail[rank];     
+          }
+        }
+      }
+
+      if ((rank % powParser(2, i)) == (powParser(2, i) - powParser(2, i - 1) - 1)) {
+        dest = rank+powParser(2,i-1);
+        if(rank!=dest){
+          origin = rank;
+          
+          mail[dest] = randoms[rank];
+          printf("Rank_%d -> Rank_%d => %d \n",origin,dest,mail[dest]);
+//rank=dest;
+        }
+      }
+      //       }
+      //   }
+     // sum += my_value;
   }
-  cilk_sync;  
-  printf("La suma es: %d\n", sum);
+
+  if(rank==size-1){printf("La suma es: %d\n", sum);}
+  }
+
+
+
 
   return 0;
 }
