0c2d4e1c0bcc6314093f769ba193459b43c4918e
jeezOC <ale.ob02@outlook.com>
2023-04-27 20:34:05 -0600

    final final?

--- CILK/init.c
+++ CILK/init.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <cilk/cilk.h>
-
-int powParser(int base, int exponent) {
-  return (int)pow((double)base, (double)exponent);
-}
+#include <time.h>
+#include <sys/time.h>
 
 int process(int rank, int size){
   int my_val=0;
@@ -17,11 +15,11 @@ int process(int rank, int size){
     int val_r =0, dest=0, origin=0;
 
     printf("Hola soy fake_rank_%d desde el worker_%d\n", rank, __cilkrts_get_worker_number());
-    
-    for (int i = 0; i <= log2(size); i++) {
-      if ((rank % powParser(2, i)) == (powParser(2, i) - 1)) {
 
-        if(origin  == (rank - powParser(2,i-1))){
+    for (int i = 0; i <= log2(size); i++) {
+      if ((rank % (int)pow(2, i)) == ((int)pow(2, i) - 1)) {
+
+        if(origin  == (rank - (int)pow(2,i-1))){
           if(dest == rank && origin !=rank){
             printf("Rank_%d <- Rank_%d => %d \n",dest,origin,val_r);
             my_val += val_r;
@@ -29,8 +27,8 @@ int process(int rank, int size){
         }
       }
 
-      if ((rank % powParser(2, i)) == (powParser(2, i) - powParser(2, i - 1) - 1)) {
-        dest = rank+powParser(2,i-1);
+      if ((rank % (int)pow(2, i)) == ((int)pow(2, i) - (int)pow(2, i - 1) - 1)) {
+        dest = rank+(int)pow(2,i-1);
         if(rank!=dest){
           origin = rank;
           printf("Rank_%d -> Rank_%d => %d \n",origin,dest,my_val);
@@ -49,14 +47,25 @@ void addition(int rank, int size){
 }
 
 int main(int argc, char *argv[]) { 
+  struct timeval start_time, end_time;
+  double elapsed_time;
+  gettimeofday(&start_time, NULL);
+
   int size =__cilkrts_get_nworkers();
-  printf("size=>%d",size);
+  printf("size=>%d\n\n",size);
 
   for(int rank=0; rank<size; rank++){
     cilk_spawn addition(rank, size);
   } 
-  cilk_sync;
 
-  printf("total =>%d \n", sum);
+  cilk_sync;
+  printf("TOTAL=> %d\n\n", sum);
+
+  gettimeofday(&end_time, NULL);
+  elapsed_time = (end_time.tv_sec - start_time.tv_sec) +
+    (end_time.tv_usec - start_time.tv_usec) / 1000000.0;
+
+  printf("Tiempo de ejecución: %f segundos\n", elapsed_time);
+
   return 0;
 }
